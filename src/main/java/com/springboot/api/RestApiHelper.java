98ebc82b68261c98a676f8e41ff2151cef90bd55
Hyemin Park <parkhyemin86@naver.com>
2018-07-04 17:04:28 +0900

    아파트 목록 API 추가

--- src/main/java/com/springboot/api/RestApiHelper.java
+++ src/main/java/com/springboot/api/RestApiHelper.java
@@ -6,14 +6,14 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
 public class RestApiHelper {
 		
 	/*"http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?ServiceKey=AI9qcEoaK35mGSnhjGyfzEBVkfoS14LZFAn7BgBQbI5FwHzxJe1%2BNwPz0GcB%2F0JsMXpFLic28nDyRorftIW8yg%3D%3D&LAWD_CD=11110&DEAL_YMD=201512"*/
 	private String _baseUrl;
 	private String _serviceKey;
-	private String _lawdCd;
-	private String _dealYmd;
+	private String _parameter = new String();
 	private String _fullUrl;
 	
 	public RestApiHelper SetBaseUrl(String baseUrl) {
@@ -26,22 +26,20 @@ public class RestApiHelper {
 		return this;
 	}
 	
-	public RestApiHelper SetLawdCd(String lawdCd) {
-		this._lawdCd = lawdCd;
+	
+	public RestApiHelper ParameterMap2Url(Map<String, String> paramMap) {
+		for(String key : paramMap.keySet()) {
+			this._parameter += "&" + key + "=" + paramMap.get(key);
+		}
 		return this;
 	}
 	
-	public RestApiHelper SetDealYmd(String dealYmd) {
-		this._dealYmd = dealYmd;
-		return this;
-	}
-	
-	public RestApiHelper GenerateFullUrl() {
-		this._fullUrl = _baseUrl + "?ServiceKey=" + this._serviceKey + "&LAWD_CD=" + this._lawdCd + "&DEAL_YMD=" + this._dealYmd;
+	public RestApiHelper GenerateApiFullUrl() {
+		this._fullUrl = _baseUrl + "?ServiceKey=" + this._serviceKey + this._parameter;
 		return this;
 	}
 
-	public String getXmlInformation()  {
+	public String getInputStream()  {
 		StringBuilder sb = new StringBuilder();
 		try {
 			URL url = new URL(_fullUrl);
--- src/main/java/com/springboot/api/SpringBoot2RestServiceApplication.java
+++ src/main/java/com/springboot/api/SpringBoot2RestServiceApplication.java
@@ -1,19 +1,15 @@
 package com.springboot.api;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.api.code.Code;
-import com.springboot.api.code.CodeHandler;
 import com.springboot.api.code.CodeRepository;
 
 import org.slf4j.Logger;
@@ -24,8 +20,13 @@ import org.slf4j.LoggerFactory;
 public class SpringBoot2RestServiceApplication {
 
 	static final Logger logger = LoggerFactory.getLogger(SpringBoot2RestServiceApplication.class);
+	// 아파트 실거래내역 API
 	private String baseUrl = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade";
 	private String serviceKey = "AI9qcEoaK35mGSnhjGyfzEBVkfoS14LZFAn7BgBQbI5FwHzxJe1%2BNwPz0GcB%2F0JsMXpFLic28nDyRorftIW8yg%3D%3D";
+	
+	// 아파트 목록 API
+	private String aptListBaseUrl = "http://apis.data.go.kr/1611000/AptListService/getLegaldongAptList";
+	private String aptListServiceKey = "AI9qcEoaK35mGSnhjGyfzEBVkfoS14LZFAn7BgBQbI5FwHzxJe1%2BNwPz0GcB%2F0JsMXpFLic28nDyRorftIW8yg%3D%3D";
 	
 	@Autowired
 	private CodeRepository codeRepository;
@@ -56,11 +57,22 @@ public class SpringBoot2RestServiceApplication {
 	public String data(@RequestParam Map<String, String> paramMap) {
 		return new RestApiHelper()
 				.SetBaseUrl(baseUrl)
+				.SetServiceKey(aptListServiceKey)
+				.ParameterMap2Url(paramMap)
+				.GenerateApiFullUrl()
+				.getInputStream();
+		
+	}
+	
+	@CrossOrigin("*")
+	@RequestMapping("/api/aptList")
+	public String aptList(@RequestParam Map<String, String> paramMap) {
+		return new RestApiHelper()
+				.SetBaseUrl(aptListBaseUrl)
 				.SetServiceKey(serviceKey)
-				.SetLawdCd(paramMap.get("lawdCd"))
-				.SetDealYmd(paramMap.get("dealYmd"))
-				.GenerateFullUrl()
-				.getXmlInformation();
+				.ParameterMap2Url(paramMap)
+				.GenerateApiFullUrl()
+				.getInputStream();
 		
 	}
 
